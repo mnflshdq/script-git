184fd03310e07e28b3cd78fe777378d9b1656590
nflshdq <nopal.sidqi@gmail.com>
2018-10-23 13:48:45 +0700

    open pdf

---
+++ latex-pdf-opener.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+green='\033[1;32m'
+function find_file()
+{
+    ack -n 'begin\{document\}' | wc -l
+}
+
+function get_tex_file_name()
+{
+    echo $(ack -n 'begin\{document\}' | cut -f 1 -d ':')
+}
+
+function get_file_name()
+{
+    echo $(ack -n 'begin\{document\}' | cut -f 1 -d ':' | cut -f 1 -d '.')
+}
+
+function open()
+{
+    evince $( get_file_name ).pdf&
+}
+
+if [ $( find_file ) -ne 0 ]; then
+    open
+else
+    cd ..
+    if [ $( find_file ) -ne 0 ]; then
+        open
+    else
+        cd ..
+        if [ $( find_file ) -ne 0 ]; then
+            open
+        else
+            echo FILE NOT FOUND
+        fi
+    fi
+fi
