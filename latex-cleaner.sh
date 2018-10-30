a655a6f0564be33301beec9731aaffb969881b94
nflshdq <nopal.sidqi@gmail.com>
2018-10-30 19:41:37 +0700

    change deleted color to green

--- latex-cleaner.sh
+++ latex-cleaner.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-find . -maxdepth 1 -type f -not -name '*.tex' -not -name '*.pdf' -not -name 'README.md' -not -name '*.cls' -delete
-echo DELETED
+# no color
+nc='\033[0m'
+# light green
+green='\033[1;32m'
+
+find . -maxdepth 1 -type f -not -name '*.tex' -not -name '*.pdf' -not -name 'README.md' -not -name '*.cls' -not -name '.gitignore' -delete
+echo "${green}DELETED${nc}"
