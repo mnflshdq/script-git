d974afde1ee774374e07fe05771e7f6dc693cd02
nflshdq <nopal.sidqi@gmail.com>
2019-01-06 16:35:26 +0700

    clean from anywhere

--- latex-cleaner.sh
+++ latex-cleaner.sh
@@ -5,5 +5,40 @@ nc='\033[0m'
 # light green
 green='\033[1;32m'
 
-find . -maxdepth 1 -type f -not -name '*.tex' -not -name '*.pdf' -not -name 'README.md' -not -name '*.cls' -not -name '.gitignore' -delete
+function delete()
+{
+find . -maxdepth 1 -type f -name '*.aux' -name '*.bcf' -name '*.log' -name '*.bbl' -name '*.blg' -name '*.out' -name '*.xml' -name '*.gz' -delete
 printf "${green}DELETED${nc}"
+}
+
+function find_file()
+{
+    ack -n 'begin\{document\}' | wc -l
+}
+
+function get_tex_file_name()
+{
+    echo $(ack -n 'begin\{document\}' | cut -f 1 -d ':')
+}
+
+function get_file_name()
+{
+    echo $(ack -n 'begin\{document\}' | cut -f 1 -d ':' | cut -f 1 -d '.')
+}
+
+cd $1
+if [ $( find_file ) -ne 0 ]; then
+    delete
+else
+    cd ..
+    if [ $( find_file ) -ne 0 ]; then
+        delete
+    else
+        cd ..
+        if [ $( find_file ) -ne 0 ]; then
+            delete
+        else
+            echo FILE NOT FOUND
+        fi
+    fi
+fi
