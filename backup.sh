74046396c67466c76c3fe1dc98173480583eaa04
nflshdq <nopal.sidqi@gmail.com>
2019-01-07 20:24:24 +0700

    deleted

--- backup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sudo rsync -aAXv / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/naufal/Public/*","/home/naufal/QRkM2dd/*","/home/naufal/Templates/*","/home/naufal/.cache/thumbnails/*","/home/lost+found"} /media/naufal/Naufal/Backup
